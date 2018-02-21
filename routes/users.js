fa82b791d39d409e1d9ec8040fbdccb73b54178d
root <root@ip-172-31-13-83.ap-northeast-2.compute.internal>
2018-02-21 09:35:03 +0000

    /users/exist GET api added

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 var mysql = require('./db_con');
-//var connection = mysql.init();
-//mysql.test_open(connection);
+var connection = mysql.init();
+mysql.test_open(connection);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-  
+  //res.send('respond with a resource');
+  if(req.query.userId != null){
+    string userId = req.query.userId; 
+  } else {
+    console.log("req error : no userId in parameter")
+  }
+
+  connection.query('SELECT * FROM USER WHERE userId=?', [userId], function(err, data){
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
+      console.log("Database result data from /users/exist?userId=" + userId);
+      console.log(data);
+
+      if(data != null){ //이미 가입한 유저
+        console.log("already joined user " + userId);
+        res.send("joinedUser");
+      } else {          //새로운 유저
+        console.log("new user " + userId);
+        res.send("newUser");
+      }
+      
+    }
+  });
 
 });
 
